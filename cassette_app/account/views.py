f5b00439cd600a57ef07c9eababaaa78efa8c410
emhaki <mhmh779@naver.com>
2024-10-02 00:48:41 +0900

    주소변경

--- cassette_app/account/views.py
+++ cassette_app/account/views.py
@@ -21,7 +21,7 @@ KAKAO_ACCESS_TOKEN_URL = 'https://kauth.kakao.com/oauth/token'
 KAKAO_USER_INFO_URL = 'https://kapi.kakao.com/v2/user/me'
 
 # 로그인 및 콜백 URL 설정 (카카오 Redirect URI와 일치해야 함)
-KAKAO_REDIRECT_URI = 'http://localhost:8000/account/kakao/callback/'
+KAKAO_REDIRECT_URI = 'http://223.130.130.103/account/kakao/callback/'
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 def kakao_login(request):
