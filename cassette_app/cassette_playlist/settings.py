fe6fe80937ca76fba9e2dcd003ff4ca2ba064ff5
emhaki <mhmh779@naver.com>
2024-10-06 04:15:45 +0900

    nginx

--- cassette_app/cassette_playlist/settings.py
+++ cassette_app/cassette_playlist/settings.py
@@ -164,6 +164,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = './.static_root/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
